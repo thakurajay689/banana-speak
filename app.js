e931383fcc97623a0aba1fdab2f8026e5ec30087
Ajay Kumar <thakurajay689@gmail.com>
2023-06-18 10:27:50 +0530

    complete

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 btnTranslate.addEventListener("click", clickHandler);
 
-function getTranslationURL(text) {
-  return serverURL + "?" + "text=" + text;
+function getTranslationURL(input) {
+  return serverURL + "?" + "text=" + input;
 }
 
-// function errorHandler(error){
-//     console.log("error occured", error);
-//     alert("something wrong with server! try again after some time")
-// }
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 function clickHandler() {
-  // outputDiv.innerText = "jsjdsbff  " + txtInput.value;
-  var inputText = txtInput.value;
+  var inputText = txtInput.value; // taking input
 
+  //calling server for processing
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText;
+      outputDiv.innerText = translatedText; // output
     })
     .catch(errorHandler);
 }
